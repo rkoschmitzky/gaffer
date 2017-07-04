4147776a88ee62b9f4028e90252e8ef01a86b184
Alex Fuller <boberfly@gmail.com>
2017-07-03 22:15:32 -0700

    No subprocess32 on windows so reverting to regular subprocess

--- python/GafferDispatch/SystemCommand.py
+++ python/GafferDispatch/SystemCommand.py
@@ -35,7 +35,10 @@
 ##########################################################################
 
 import os
-import subprocess32 as subprocess
+if os.name == 'posix' and sys.version_info[0] < 3:
+    import subprocess32 as subprocess
+else:
+    import subprocess
 
 import IECore
 
