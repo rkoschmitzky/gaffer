cd3b9dca56b4d85cdf9f783022d7150fbc1c2ce6
Alex Fuller <boberfly@gmail.com>
2017-12-27 20:59:40 -0800

    'and' to '&&'

--- src/GafferUI/StandardGraphLayout.cpp
+++ src/GafferUI/StandardGraphLayout.cpp
@@ -1422,7 +1422,7 @@ size_t StandardGraphLayout::unconnectedInputPlugs( NodeGadget *nodeGadget, std::
 	plugs.clear();
 	for( RecursiveInputPlugIterator it( nodeGadget->node() ); !it.done(); it++ )
 	{
-		if( (*it)->getInput() == nullptr and nodeGadget->nodule( it->get() ) )
+		if( (*it)->getInput() == nullptr && nodeGadget->nodule( it->get() ) )
 		{
 			plugs.push_back( it->get() );
 		}
