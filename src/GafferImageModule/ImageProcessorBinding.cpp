5b06e71feb45a588ddc2441f100c857ca6084245
Alex Fuller <boberfly@gmail.com>
2017-12-27 20:59:00 -0800

    Using std::numeric_limits instead of Imath::limits due to MSVC linkage problem
    Representative error from MSVC:
    error LNK2019: unresolved external symbol "public: static unsigned __int64 __cdecl Imath_2_2::limits<unsigned __int64>::max(void)" (?m
    ax@?$limits@_K@Imath_2_2@@SA_KXZ) referenced in function "public: void __cdecl Gaffer::Switch<class Gaffer::ComputeNode>::setup(class Gaffer::Plug co
    nst *)"

--- src/GafferImageModule/ImageProcessorBinding.cpp
+++ src/GafferImageModule/ImageProcessorBinding.cpp
@@ -32,6 +32,8 @@
 //
 //////////////////////////////////////////////////////////////////////////
 
+#include <limits>
+
 #include "boost/python.hpp"
 
 #include "ImageProcessorBinding.h"
@@ -62,7 +64,7 @@ void GafferImageModule::bindImageProcessor()
 				(
 					arg( "name" ) = GraphComponent::defaultName<ImageProcessor>(),
 					arg( "minInputs" ),
-					arg( "maxInputs" ) = Imath::limits<size_t>::max()
+					arg( "maxInputs" ) = std::numeric_limits<size_t>::max()
 				)
 			)
 		)
