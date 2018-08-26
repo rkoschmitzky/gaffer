5ee4212358a7752ee30c7429cc641fb65beb77a1
Eric Mehl <eric@thinkhypothetical.com>
2018-08-26 17:21:51 -0400

    use full path text to include the root, which on Windows may be the drive letter

--- src/GafferUIModule/PathListingWidgetBinding.cpp
+++ src/GafferUIModule/PathListingWidgetBinding.cpp
@@ -989,7 +989,7 @@ IECore::PathMatcher getSelection( uint64_t treeViewAddress )
 	IECore::PathMatcher result;
 	for( const auto &index : selectedIndices )
 	{
-		result.addPath( model->pathForIndex( index )->names() );
+		result.addPath( model->pathForIndex( index )->string() );
 	}
 	return result;
 }
