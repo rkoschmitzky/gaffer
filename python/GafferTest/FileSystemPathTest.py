54314129d745edd77e5fa3837bb1d92f5702d1c6
Eric Mehl <eric@thinkhypothetical.com>
2018-09-30 11:03:03 -0400

    add simple test to check truncating

--- python/GafferTest/FileSystemPathTest.py
+++ python/GafferTest/FileSystemPathTest.py
@@ -375,6 +375,13 @@ class FileSystemPathTest( GafferTest.TestCase ) :
 		c = p.children()
 		self.assertEqual( len( c ), 8 )
 
+	def testTruncateUntilValid( self ):
+
+		p = Gaffer.FileSystemPath( "T" )
+		print str( p )
+		print str (p.truncateUntilValid() )
+		self.assertEqual( str( p.truncateUntilValid() ), "" )
+
 	def setUp( self ) :
 
 		GafferTest.TestCase.setUp( self )
