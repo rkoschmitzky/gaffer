3a8c763cec43cfae9c7295de61320caf4656e958
Eric Mehl <eric@thinkhypothetical.com>
2018-12-03 19:26:25 -0500

    load test files cross-platform

--- python/GafferImageTest/ImageReaderTest.py
+++ python/GafferImageTest/ImageReaderTest.py
@@ -48,12 +48,16 @@ import GafferImageTest
 
 class ImageReaderTest( GafferImageTest.ImageTestCase ) :
 
-	fileName = os.path.expandvars( "$GAFFER_ROOT/python/GafferImageTest/images/circles.exr" )
-	colorSpaceFileName = os.path.expandvars( "$GAFFER_ROOT/python/GafferImageTest/images/circles_as_cineon.exr" )
-	offsetDataWindowFileName = os.path.expandvars( "$GAFFER_ROOT/python/GafferImageTest/images/rgb.100x100.exr" )
-	jpgFileName = os.path.expandvars( "$GAFFER_ROOT/python/GafferImageTest/images/circles.jpg" )
-
 	def setUp( self ) :
+		if os.name == "nt":
+			gaffer_root = os.path.expandvars( "%GAFFER_ROOT%" )
+		else:
+			gaffer_root = os.path.expandvars( "$GAFFER_ROOT" )
+
+		fileName = Gaffer.FileSystemPath( gaffer_root + "/python/GafferImageTest/images/circles.exr" ).nativeString()
+		colorSpaceFileName = Gaffer.FileSystemPath( gaffer_root + "$GAFFER_ROOT/python/GafferImageTest/images/circles_as_cineon.exr" ).nativeString()
+		offsetDataWindowFileName = Gaffer.FileSystemPath( gaffer_root + "$GAFFER_ROOT/python/GafferImageTest/images/rgb.100x100.exr" ).nativeString()
+		jpgFileName = Gaffer.FileSystemPath( gaffer_root + "$GAFFER_ROOT/python/GafferImageTest/images/circles.jpg" ).nativeString()
 
 		GafferImageTest.ImageTestCase.setUp( self )
 		self.__defaultColorSpaceFunction = GafferImage.ImageReader.getDefaultColorSpaceFunction()
